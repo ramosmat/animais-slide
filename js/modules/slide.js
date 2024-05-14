581fc55a16c239c60539e3b4d4f78ee129a2bc8b
Matheus <matheusramosbi.13@gmail.com>
2024-05-14 15:12:41 -0300

    prev & next arrow navegation

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,6 +1,6 @@
 import debounce from "./debouce.js";
 
-export default class Slide {
+export class Slide {
   constructor(slide, wraper) {
     this.slide = document.querySelector(slide);
     this.wraper = document.querySelector(wraper);
@@ -145,6 +145,9 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
     this.onResize = debounce(this.onResize.bind(this), 200);
+
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
   }
 
   addSlideEvents() {
@@ -160,6 +163,20 @@ export default class Slide {
     this.addSlideEvents();
     this.slidesConfig();
     this.addResizeEvent();
+    this.changeSlide(0);
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
   }
 }
