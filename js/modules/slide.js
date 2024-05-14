c7361c0e7d308e18de0e2bbc0dc388539fe8cc85
Matheus <matheusramosbi.13@gmail.com>
2024-05-14 12:41:47 -0300

    add touch events

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -9,42 +9,62 @@ export default class Slide {
     };
   }
 
-  moveSlide(distX) {
-    this.dist.movePosition = distX;
-    this.slide.style.transform = `translate3D(${distX}px, 0, 0)`;
+  //Evento que será executado quando o usuário clicar e mover a imagem
+  //Verifica se o tipo de evento é feito pelo computador ou celular e chama a onMove
+  onStart(event) {
+    let moveType;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      moveType = "mousemove";
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      moveType = "touchmove";
+    }
+    this.wraper.addEventListener(moveType, this.onMove);
   }
 
+  //Evento que é executado sempre que o usuário movimentar o mouse
+  //Pega a posição do clique de acordo com o tipo de evento
+  onMove(event) {
+    const pointerPosition =
+      event.type === "mousemove"
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
+    this.moveSlide(finalPosition);
+  }
+
+  //Evento que é executado pelo onMove para atualizar a posição do slide
   updatePosition(clientX) {
     this.dist.movement = (this.dist.startX - clientX) * 1.3;
     return this.dist.finalPosition - this.dist.movement;
   }
 
-  //Evento que será executado quando o usuário clicar na imagem
-  onStart(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wraper.addEventListener("mousemove", this.onMove);
+  //Evento que é executado pelo onMove para aplicar o movimento do slide
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3D(${distX}px, 0, 0)`;
   }
 
-  onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
-    this.moveSlide(finalPosition);
-  }
-
+  //Evento que é executado quando o usuário soltar o clique do mouse
   onEnd(event) {
-    this.wraper.removeEventListener("mousemove", this.onMove);
+    const movetype = event.type === "mouseup" ? "mousemove" : "touchmove";
+    this.wraper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
-  }
-
-  addSlideEvents() {
-    this.wraper.addEventListener("mousedown", this.onStart);
-    this.wraper.addEventListener("mouseup", this.onEnd);
   }
 
   bindEvents() {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+  }
+
+  addSlideEvents() {
+    this.wraper.addEventListener("mousedown", this.onStart);
+    this.wraper.addEventListener("touchstart", this.onStart);
+    this.wraper.addEventListener("touchend", this.onEnd);
+    this.wraper.addEventListener("mouseup", this.onEnd);
   }
 
   init() {
